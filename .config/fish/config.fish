73cdc78776d11f7fe78f59cea215a92289d8a3dd
Guizmo <moguayv@gmail.com>
2024-03-17 08:49:00 +0100

    Update config.fish
    Add check if brew is installed to prevent error

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+command -v /home/linuxbrew/.linuxbrew/bin/brew >/dev/null 2>&1 && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
 starship init fish | source
 zoxide init fish | source
 
